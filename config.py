aa46a26924cfca4c063dd79eff78f143a93171a2
muztishini <111636144+muztishini@users.noreply.github.com>
2025-12-08 13:59:08 +0500

    add mysql

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-USER = "testuser"
-PASSWORD = "123456"
-HOST = "localhost"
-DATABASE = "todo"
+POST_USER = "testuser"
+POST_PASSWORD = "123456"
+POST_HOST = "localhost"
+POST_DATABASE = "todo"
+
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "12345"
+MYSQL_HOST = "localhost"
+MYSQL_DATABASE = "todo"
